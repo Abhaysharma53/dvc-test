b285c93887fb2216b3c8596468dc3e396fd83fae
Abhay <abhaysharma.cs2017.pgdav@gmail.com>
2024-08-20 02:44:25 +0530

    2nd modification done

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,6 +4,8 @@ import os
 
 data = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
 data = data.iloc[:, 3:]
+data.drop(columns=['Avg. Session Length'],inplace=True)
 
-data = data[data['Length of Membership'] > 3]
+
+data = data[data['Length of Membership'] > 1]
 data.to_csv(os.path.join('data', 'customer.csv'))
